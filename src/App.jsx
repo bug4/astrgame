f341c514930a710ca9a840aa715e50e6e94a9922
bug4 <bugad22@gmail.com>
2025-04-28 23:51:16 +0300

    Ready for deployment

--- src/App.jsx
+++ src/App.jsx
@@ -132,7 +132,7 @@ function App() {
 
           {/* Buy Button */}
           <a 
-            href="https://letsbonk.fun/" 
+            href="https://letsbonk.fun/token/G3v9e6DFHixpUDDYDFvCrd8uAvPWjvi223unYSRGbonk" 
             target="_blank" 
             rel="noopener noreferrer" 
             className="group relative flex items-center gap-3 bg-gradient-to-r from-cyan-500 to-blue-500 px-8 py-4 rounded-xl text-white font-bold shadow-lg hover:shadow-cyan-500/20 transition-all hover:-translate-y-0.5"
